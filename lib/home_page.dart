dbe22d81a577e6987d1dcf3834f6d1ce3f2b09e3
Sulaiman <leman109447584@gmail.com>
2025-01-24 11:58:47 +0700

    add header

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,6 +5,42 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          margin: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Image.asset("assets/images/plantify_icon.png"),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "PLANTIFY",
+                    style: TextStyle(fontSize: 24, fontFamily: "Philosopher"),
+                  ),
+                  Spacer(),
+                  Transform.scale(
+                      scale: 1.3,
+                      child:
+                          Image.asset("assets/images/notification_icon.png")),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Transform.scale(
+                      scale: 1.3,
+                      child: Image.asset("assets/images/widgets.png"))
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
